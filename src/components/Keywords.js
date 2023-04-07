64433e21785434a92d7cdfd58efff7860ff40e67
Maria Luiza <maria.stellet@olxbr.com>
2023-04-07 12:11:09 -0300

    fix keyword component

--- src/components/Keywords.js
+++ src/components/Keywords.js
@@ -5,13 +5,17 @@ import {
   InfoWrapper,
 } from "./CommonStyledComponets";
 import styled from "styled-components";
-import { Table, TableHead, TagCell, Content, ContentCell } from "./Headings";
+import { Table, TableHead } from "./Headings";
 import { handleIconInformation } from "../utils/InformationContainerHandler";
 
+const Cell = styled.p`
+  margin-bottom: 10px;
+  color: #4A4A4A;
+  font-size: 16px;
+`
 
 export const Keywords = ({ data }) => {
   const isArrayEmpty = data.length === 1 && !data.content
-  console.log(">>>", data)
 
   return (
     <div>
@@ -20,23 +24,29 @@ export const Keywords = ({ data }) => {
       <DefinitionWrapper>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ligula ante, et eleifend lorem molestie quis. Nulla dictum rhoncus facilisis.
       </DefinitionWrapper>
-   {isArrayEmpty && (
+      {isArrayEmpty && (
         <InfoWrapper style={{ marginLeft: '0px' }} type='error'>
           {handleIconInformation('error')}
           {data[0].errorName}
         </InfoWrapper>
       )}
-         {!isArrayEmpty && (
-        <Table>
-          <TableHead>
-            <th style={{ maxWidth: '30%' }}>Palavra-chave</th>
-            <th style={{ width: '70%' }}>Quantidade encontrada</th>
-            <th style={{ width: '70%' }}>Densidade</th>
+      {!isArrayEmpty && (
+        <div style={{width:  '100%', display: 'flex', justifyContent: 'center'}}>
+        <Table style={{ width: '60%' }}>
+          <TableHead >
+            <th style={{ width: '33%'}}>Palavra-chave</th>
+            <th style={{ width: '33%' }}>Quantidade encontrada</th>
+            <th style={{ width: '33%' }}>Densidade</th>
           </TableHead>
-          <td>a</td>
-          <td>b</td>
-          <td>c</td>
+          {data.map(item => (
+            <tr>
+              <td><Cell>{item.keyword}</Cell></td>
+              <td><Cell>{item.count}</Cell></td>
+              <td><Cell>{item.density}</Cell></td>
+            </tr>
+          ))}
         </Table>
+        </div>
       )}
     </div>
   )
